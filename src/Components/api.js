b7a059eb7094a9d6368e402e5f141c34cbbe0b98
jatinmundra18 <42213274+jatinmundra18@users.noreply.github.com>
2021-03-30 14:57:36 +0530

    Update api.js
    I have taken 'joke' just for testing as I was trying to reflect a tutorial.

--- src/Components/api.js
+++ src/Components/api.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 
 
 function Api() {
-  const [joke, setJoke] = useState("");
+  const [joke, setJoke] = useState("");//taken 'joke' for 'volume just for reference
 
   const getJoke = () => {
     Axios.get("https://api.thinger.io/v1/users/luthra1411/devices/NODEMCU/tokens?authorization=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXYiOiJOT0RFTUNVIiwiaWF0IjoxNjEzMzY2MjU1LCJqdGkiOiI2MDJhMDNlZmRlNWM3YzA4ZjYwYjg1YWIiLCJzdnIiOiJhcC1zb3V0aGVhc3QuYXdzLnRoaW5nZXIuaW8iLCJ1c3IiOiJsdXRocmExNDExIn0.OXHD5DeQ72czY37KH9fUnRbjDJQRRIhdrgbep98iboI").then(
